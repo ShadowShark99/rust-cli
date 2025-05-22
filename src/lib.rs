c4aa10a9cb3a421d7888536474569a0a1ec26a23
ShadowShark99 <kadenwchan@gmail.com>
2025-05-22 08:57:42 -0500

    replace clone with iterator for speedup

--- src/lib.rs
+++ src/lib.rs
@@ -26,20 +26,41 @@ pub struct Config{
 }
 
 impl Config{
-  pub fn build( args: &[String]) -> Result<Config, &'static str> {
-      let mut ignore_case = env::var("IGNORE_CASE").is_ok();
-      if args.len() < 3 || args.len() > 4 {
-          return Err("Usage: minigrep <query> <file_path> [--ignore-case]");
-          //std::process::exit(1);
-      }
-      else if args.len() == 4{
-        // command line arg has higher precedence than env var
-          ignore_case = args[3] == "--ignore-case" || args[3] == "1";
-      }
+  pub fn build( 
+    mut args: impl Iterator<Item = String>,
+  ) -> Result<Config, &'static str> {
+      //let mut ignore_case = env::var("IGNORE_CASE").is_ok();
+      // if args.len() < 3 || args.len() > 4 {
+      //     return Err("Usage: minigrep <query> <file_path> [--ignore-case]");
+      //     //std::process::exit(1);
+      // }
+      // else if args.len() == 4{
+      //   // command line arg has higher precedence than env var
+      //     ignore_case = args[3] == "--ignore-case" || args[3] == "1";
+      // }
+      //scan command arg
+      args.next();
+      let query = match args.next(){
+        Some(arg) => arg,
+        None => panic!("no query entered"),
+        
+      };
+
+      let file_path = match args.next(){
+        Some(arg) => arg,
+        None => panic!("no file_path entered"),
+      };
+
+      let ignore_case = match args.next() {
+        Some(arg) => arg == "--ignore-case" || arg == "1",
+        None => env::var("IGNORE_CASE").is_ok(),
+      };
+
+
   
       Ok(Config{
-          query: args[1].clone(),
-          file_path: args[2].clone(),
+          query,
+          file_path,
           ignore_case,
       })
   }
